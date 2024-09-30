991ba4a13057924b5b24c1e7f1dc92814ca385c1
Felipe García <L21TE0645@teziutlan.tecnm.mx>
2024-09-30 12:52:54 -0600

    Update LoginController.java

--- segunda-vida-backend/src/main/java/com/servicios_web/segunda_vida_backend/Controller/LoginController.java
+++ segunda-vida-backend/src/main/java/com/servicios_web/segunda_vida_backend/Controller/LoginController.java
@@ -1,10 +1,12 @@
-import org.springframework.stereotype.Controller;
+package com.servicios_web.segunda_vida_backend.Controller;
+
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 public class LoginController {
 
     @GetMapping("/login")
