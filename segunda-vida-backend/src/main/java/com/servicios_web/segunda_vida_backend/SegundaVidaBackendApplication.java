0816cac15992beac1937a8de3a79e3fc4949bf18
Felipe García <L21TE0645@teziutlan.tecnm.mx>
2024-09-30 12:20:43 -0600

    Inicializador de Spring boot

---
+++ segunda-vida-backend/src/main/java/com/servicios_web/segunda_vida_backend/SegundaVidaBackendApplication.java
@@ -0,0 +1,13 @@
+package com.servicios_web.segunda_vida_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SegundaVidaBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SegundaVidaBackendApplication.class, args);
+	}
+
+}
---
+++ segunda-vida-backend/src/test/java/com/servicios_web/segunda_vida_backend/SegundaVidaBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.servicios_web.segunda_vida_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SegundaVidaBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
