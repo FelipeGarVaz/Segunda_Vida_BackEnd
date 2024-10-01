ada3152122d5b3bf1a91e9e1f82c73b2347818bf
Felipe García <L21TE0645@teziutlan.tecnm.mx>
2024-10-01 12:49:25 -0600

    Complementos

--- segunda-vida-backend/src/main/java/com/servicios_web/segunda_vida_backend/Controller/LoginController.java
+++ segunda-vida-backend/src/main/java/com/servicios_web/segunda_vida_backend/Controller/LoginController.java
@@ -1,12 +1,12 @@
 package com.servicios_web.segunda_vida_backend.Controller;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 public class LoginController {
 
     @GetMapping("/login")
